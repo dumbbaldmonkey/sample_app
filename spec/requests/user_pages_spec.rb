8787ec199662a7a7e434817f2352923b0e66890f
Jared Doyle <Jared.Doyle@LibertyMutual.com>
2014-03-07 11:38:22 -0500

    Signin and Signout functionality added

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,18 +13,46 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-    end
+	  describe "after submission" do
+        before { click_button submit }
 
-    describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+		it { should have_selector('div.alert.alert-error') }
+        it { should have_title('Sign Up') }
+        it { should have_content('errors') }
+		it { should have_content('Name can\'t be blank') }
+		it { should have_content('Email can\'t be blank') }
+		it { should have_content('Password can\'t be blank') }
+		it { should have_content('Email is invalid') }
+		it { should have_content('Password is too short') }
       end
+    end
+    describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        
+		describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: 'user@example.com') }
+  
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
+		
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+		
+		describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: 'user@example.com') }
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+          it { should have_link('Sign out') }
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
